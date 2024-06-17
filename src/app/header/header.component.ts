648e38c893bc31d684af1f0e1f0f17d2ed03261c
Mathieu <mathieu.barbey18@gmail.com>
2024-06-17 17:52:06 +0200

    navbar component created

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NavbarComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  constructor( private router: Router ) { }
 }
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+  
+  constructor( private router: Router ) { }
+
+}
